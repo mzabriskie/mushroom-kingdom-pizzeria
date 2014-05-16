dad7907c375c65fef99a28ed27760a94c0d5eca4
Matt Zabriskie <mzabriskie@gmail.com>
2014-05-16 15:19:26 -0600

    Detecting locale

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ var acceptLang = require('accept-language'),
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.use(function (req, res, next) {
+	var parsed = acceptLang.parse(req.header('Accept-Language')),
+		locale = parsed[0];
+
+	res.locals.locale = locale.language + (locale.region ? '-' + locale.region : '');
+	next();
+});
+
 app.get('/', function (req, res) { res.render('home'); });
 
 app.listen(3000);
